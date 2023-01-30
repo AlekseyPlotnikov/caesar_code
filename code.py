3a577e14ab6c92b3b77a05398d2f299316264c74
aleksey <Deep1985@yandex.ru>
2023-01-30 23:07:33 +0300

    caesar_code()

--- code.py
+++ code.py
@@ -88,3 +88,29 @@ def decryption_eng2(str, shfr):
         else:
             text.append(i)
     return ''.join(text).capitalize()
+
+
+def caesar_code():
+    print('Приветсявую в программе шифр Цезаряю')
+    while True:
+        choice = input('Вы желаете зашифровать [ш] или дешифровать текст [д]?\n').lower() == 'д'
+        language = input('На каком языке будет ваш текст? [ру/eng]\n').lower() == 'ру'
+        shfr = int(input('На какой сдвиг вправо?\n'))
+        text = input('Ведите текст:\n')
+        if choice:
+            if language:
+                print(decryption_ru2(text, shfr))
+            else:
+                print(decryption_eng2(text, shfr))
+        else:
+            if language:
+                print(encryption_ru2(text, shfr))
+            else:
+                print(encryption_eng2(text, shfr))
+        a = input('Будет ли ещё текст?[да/нет]\n')
+        if a.lower() != 'да':
+            print('Пока!')
+            break
+
+
+caesar_code()
